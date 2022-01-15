ec5f2dc6d8f37761927031c01ee16f58de4ac006
Mihir Chaturvedi <plibither8@gmail.com>
2022-01-15 11:27:43 +0530

    refactor(pkg): Cleaner code

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,5 @@
-import { readFile, writeFile } from "fs/promises";
-import path from "path";
-import { xml2js } from "xml-js";
 import { getBounds, getDistance, getDistanceFromLine } from "geolib";
-
-type BoundingCoordinates = [Coordinates, Coordinates, Coordinates, Coordinates];
-
-type Line = [Coordinates, Coordinates];
+import { xml2js } from "xml-js";
 
 interface XmlValue {
   _text: string;
@@ -51,114 +45,155 @@ export interface Coordinates {
   longitude: number;
 }
 
+type Line = [Coordinates, Coordinates];
+
 export interface GenerateSvgOptions {
   smooth?: boolean;
+  smoothing?: number;
+  accuracy?: number;
+  svg?: Partial<{
+    width: number;
+    height: number;
+    stroke: string;
+    strokeWidth: number;
+    strokeLinecap: string;
+    strokeMiterlimit: number;
+    fill: string;
+  }>;
 }
 
-const line = (
-  pointA: CartesianPoint,
-  pointB: CartesianPoint
-): {
-  length: number;
-  angle: number;
-} => {
-  const lengthX = pointB.x - pointA.x;
-  const lengthY = pointB.y - pointA.y;
-  return {
-    length: Math.sqrt(Math.pow(lengthX, 2) + Math.pow(lengthY, 2)),
-    angle: Math.atan2(lengthY, lengthX),
-  };
+const defaultOptions: Required<GenerateSvgOptions> = {
+  smooth: true,
+  smoothing: 0.2,
+  accuracy: 0.001,
+  svg: {
+    stroke: "red",
+    strokeWidth: 4,
+    strokeLinecap: "round",
+    strokeMiterlimit: 4,
+    fill: "none",
+  },
 };
 
-const controlPoint = (
-  current: CartesianPoint,
-  previous: CartesianPoint,
-  next: CartesianPoint,
-  reverse?: boolean
-): CartesianPoint => {
-  const p = previous || current;
-  const n = next || current;
-  const smoothing = 0.2;
-  const o = line(p, n);
-  const angle = o.angle + (reverse ? Math.PI : 0);
-  const length = o.length * smoothing;
-  const x = current.x + Math.cos(angle) * length;
-  const y = current.y + Math.sin(angle) * length;
-  return { x, y };
-};
+const getOptions = (
+  supplied: GenerateSvgOptions
+): Required<GenerateSvgOptions> => ({
+  ...defaultOptions,
+  ...supplied,
+});
 
-const bezier = (
-  point: CartesianPoint,
-  index: number,
-  points: CartesianPoint[]
+const getSvgPath = (
+  points: CartesianPoint[],
+  smooth: boolean,
+  smoothing: number
 ) => {
-  const { x: cpsX, y: cpsY } = controlPoint(
-    points[index - 1],
-    points[index - 2],
-    point
-  );
-  const { x: cpeX, y: cpeY } = controlPoint(
-    point,
-    points[index - 1],
-    points[index + 1],
-    true
-  );
-  return `C ${cpsX},${cpsY} ${cpeX},${cpeY} ${point.x},${point.y}`;
-};
+  const controlPoint = (
+    current: CartesianPoint,
+    previous?: CartesianPoint,
+    next?: CartesianPoint,
+    reverse?: boolean
+  ): CartesianPoint => {
+    previous ??= current;
+    next ??= current;
+    const lengthX = next.x - previous.x;
+    const lengthY = next.y - previous.y;
+    const angle = Math.atan2(lengthY, lengthX) + (reverse ? Math.PI : 0);
+    const length =
+      Math.sqrt(Math.pow(lengthX, 2) + Math.pow(lengthY, 2)) * smoothing;
+    return {
+      x: current.x + Math.cos(angle) * length,
+      y: current.y + Math.sin(angle) * length,
+    };
+  };
+  const curved = (point: CartesianPoint, index: number) => {
+    const { x: cpsX, y: cpsY } = controlPoint(
+      points[index - 1],
+      points[index - 2],
+      point
+    );
+    const { x: cpeX, y: cpeY } = controlPoint(
+      point,
+      points[index - 1],
+      points[index + 1],
+      true
+    );
+    return `C ${cpsX},${cpsY} ${cpeX},${cpeY} ${point.x},${point.y}`;
+  };
+  const straight = (point: CartesianPoint) => `L ${point.x},${point.y}`;
 
-const getSvgPath = (points: CartesianPoint[], smooth?: boolean) =>
-  points.reduce(
-    (acc, point, index, array) =>
+  return points.reduce(
+    (acc, point, index) =>
       index === 0
         ? `M ${point.x},${point.y}`
-        : `${acc} ${
-            smooth ? bezier(point, index, array) : `L ${point.x},${point.y}`
-          }`,
+        : `${acc} ${smooth ? curved(point, index) : straight(point)}`,
     ""
   );
+};
 
 const getSvg = (
   width: number,
   height: number,
   path: string,
-  options?: {}
+  options?: GenerateSvgOptions["svg"]
 ): string =>
-  `
-    <svg xmlns="http://www.w3.org/2000/svg" width="${width}" height="${height}" viewBox="0 0 ${width} ${height}">
-      <path d="${path}" fill="none" stroke="red" stroke-width="4" stroke-linecap="round" stroke-miterlimit="4"/>
-    </svg>
-  `.trim();
+  `<svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="${options?.width ?? width}"
+      height="${options?.height ?? height}"
+      viewBox="0 0 ${options?.width ?? width} ${options?.height ?? height}"
+    >
+      <path
+        d="${path}"
+        fill="${options?.fill ?? "none"}"
+        stroke="${options?.stroke ?? "red"}"
+        stroke-width="${options?.strokeWidth ?? "4"}"
+        stroke-linecap="${options?.strokeLinecap ?? "round"}"
+        stroke-miterlimit="${options?.strokeMiterlimit ?? "4"}"
+      />
+    </svg>`.trim();
 
-const getBoundingCoordinates = (bounds: Bounds): BoundingCoordinates => [
-  {
-    latitude: bounds.maxLat,
-    longitude: bounds.minLng,
-  },
-  {
-    latitude: bounds.maxLat,
-    longitude: bounds.maxLng,
-  },
-  {
-    latitude: bounds.minLat,
-    longitude: bounds.maxLng,
-  },
-  {
-    latitude: bounds.minLat,
-    longitude: bounds.minLng,
-  },
-];
+const getAxes = (
+  points: Coordinates[]
+): {
+  x: Line;
+  y: Line;
+} => {
+  const bounds = getBounds(points) as Bounds;
+  const corners: Record<string, Coordinates> = {
+    nw: {
+      latitude: bounds.maxLat,
+      longitude: bounds.minLng,
+    },
+    ne: {
+      latitude: bounds.maxLat,
+      longitude: bounds.maxLng,
+    },
+    se: {
+      latitude: bounds.minLat,
+      longitude: bounds.maxLng,
+    },
+    sw: {
+      latitude: bounds.minLat,
+      longitude: bounds.minLng,
+    },
+  };
+  return {
+    x: [corners.nw, corners.ne],
+    y: [corners.nw, corners.sw],
+  };
+};
 
 const getCartesianPoint = (
   point: Coordinates,
   xAxis: Line,
-  yAxis: Line
-): CartesianPoint => {
-  const x = getDistanceFromLine(point, ...yAxis, 0.001);
-  const y = getDistanceFromLine(point, ...xAxis, 0.001);
-  return { x, y };
-};
+  yAxis: Line,
+  accuracy: number
+): CartesianPoint => ({
+  x: getDistanceFromLine(point, ...yAxis, accuracy),
+  y: getDistanceFromLine(point, ...xAxis, accuracy),
+});
 
-export function generateSvgFromGpx(gpx: string, options?: GenerateSvgOptions) {
+export function fromGpx(gpx: string) {
   const parsedGpx = xml2js(gpx, {
     compact: true,
     ignoreDeclaration: true,
@@ -169,38 +204,48 @@ export function generateSvgFromGpx(gpx: string, options?: GenerateSvgOptions) {
       longitude: Number(_attributes.lon),
     })
   );
-  return generateSvg(
-    points.filter((_, index) => !(index % 3)),
-    options
-  );
+  return outputs(points);
 }
 
 export function generateSvg(
   points: Coordinates[],
-  options?: GenerateSvgOptions
+  options: GenerateSvgOptions
 ): string {
-  const bounds = getBounds(points) as Bounds;
-  const boundingCoordinates = getBoundingCoordinates(bounds);
-  const xAxis: Line = [boundingCoordinates[0], boundingCoordinates[1]];
-  const yAxis: Line = [boundingCoordinates[0], boundingCoordinates[3]];
-  const width = getDistance(...xAxis);
-  const height = getDistance(...yAxis);
+  const { x: xAxis, y: yAxis } = getAxes(points);
+  const width = getDistance(...xAxis, options.accuracy);
+  const height = getDistance(...yAxis, options.accuracy);
   const cartesianPoints = points.map((point) =>
-    getCartesianPoint(point, xAxis, yAxis)
+    getCartesianPoint(point, xAxis, yAxis, options.accuracy)
   );
-  return getSvg(width, height, getSvgPath(cartesianPoints, options?.smooth));
+  return getSvg(
+    width,
+    height,
+    getSvgPath(cartesianPoints, options.smooth, options.smoothing),
+    options.svg
+  );
 }
 
 export function generateSvgPath(
   points: Coordinates[],
-  options?: GenerateSvgOptions
+  options: GenerateSvgOptions
 ): string {
-  const bounds = getBounds(points) as Bounds;
-  const boundingCoordinates = getBoundingCoordinates(bounds);
-  const xAxis: Line = [boundingCoordinates[0], boundingCoordinates[1]];
-  const yAxis: Line = [boundingCoordinates[0], boundingCoordinates[3]];
+  const { x: xAxis, y: yAxis } = getAxes(points);
   const cartesianPoints = points.map((point) =>
-    getCartesianPoint(point, xAxis, yAxis)
+    getCartesianPoint(point, xAxis, yAxis, options.accuracy)
   );
-  return getSvgPath(cartesianPoints, options?.smooth);
+  return getSvgPath(cartesianPoints, options.smooth, options.smoothing);
 }
+
+export const inputs = {
+  fromGpx,
+  fromCoordinates: (points: Coordinates[]) => outputs(points),
+};
+
+const outputs = (points: Coordinates[]) => ({
+  toSvg: (options?: GenerateSvgOptions) =>
+    generateSvg(points, getOptions(options)),
+  toSvgPath: (options?: GenerateSvgOptions) =>
+    generateSvgPath(points, getOptions(options)),
+});
+
+export default inputs;
